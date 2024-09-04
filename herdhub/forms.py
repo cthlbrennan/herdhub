38cba3ca999f27f40e54c3896723583f6d27cb1c
Cathal Brennan <cthlbrennan@gmail.com>
2024-09-04 18:53:33 +0000

    Add extra fields to AddNewCow form

--- herdhub/forms.py
+++ herdhub/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import BreedChoices
+from .models import BreedChoices, HealthChoices, PregnancyStatus
 
 class AddCowForm(forms.Form):
     registration_number = forms.CharField(label='Registration Number')
     dob = forms.DateField(label='Date of Birth')
     breed = forms.ChoiceField(label='Breed', choices=BreedChoices.choices)
+    health_status = forms.ChoiceField(label='Health Status', choices=HealthChoices.choices)    
+    pregnancy_status = forms.ChoiceField(label='Pregnancy Status', choices=PregnancyStatus.choices)
+    number_of_calvings = forms.IntegerField(label='Number of Times Calved')
+    last_calving_date = forms.DateField(label='Last Calving Date')
+    milk_production = forms.IntegerField(label='Milk Produced Per Annum')
+    comments = forms.CharField(label='Comments')
