25f08881e54cb4a0608093f4ff29119a1a3696cb
Cathal Brennan <cthlbrennan@gmail.com>
2024-09-09 10:59:04 +0000

    Add date picker widgets to forms

--- herdhub/forms.py
+++ herdhub/forms.py
@@ -7,11 +7,20 @@ class AddCowForm(forms.ModelForm):
         fields = ['registration_number', 'dob', 'breed', 'health_status', 
                   'pregnancy_status', 'number_of_calvings', 'last_calving_date', 
                   'milk_production', 'comments']
+        widgets = {
+            'dob': forms.DateInput(attrs={'type': 'date'}),
+        }
+        widgets = {
+            'last_calving_date': forms.DateInput(attrs={'type': 'date'}),
+        }
     
 class AddBullForm(forms.ModelForm):
     class Meta:
         model = Bull
         fields = ['registration_number', 'dob', 'breed', 'health_status', 'comments']
+        widgets = {
+            'dob': forms.DateInput(attrs={'type': 'date'}),
+        }
 
 class AddBreedingForm(forms.ModelForm):
     class Meta:
@@ -20,6 +29,7 @@ class AddBreedingForm(forms.ModelForm):
         widgets = {
             'breeding_date': forms.DateInput(attrs={'type': 'date'}),
         }
+
     def __init__(self, *args, **kwargs):
         # Expecting the user to be passed as a keyword argument
         user = kwargs.pop('user', None)
@@ -34,7 +44,9 @@ class AddCalfForm(forms.ModelForm):
     class Meta:
         model = Calf
         fields = ['registration_number', 'dob', 'breeding', 'sex', 'calving_method', 'comments']
-
+        widgets = {
+            'dob': forms.DateInput(attrs={'type': 'date'}),
+        }
 class SendMessageForm(forms.ModelForm):
     class Meta:
         model = Message
