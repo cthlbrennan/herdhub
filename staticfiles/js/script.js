3f0ec8909b0810bea987ee7cc04a9f0882e65fcf
Cathal Brennan <cthlbrennan@gmail.com>
2024-09-20 18:22:15 +0000

    Collect static files before Heroku deployment

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,27 +1,35 @@
 /* jshint esversion: 11 */
 
 document.addEventListener('DOMContentLoaded', function() {
-    // dynamically change the hero-image background depending on page (home or about)
+    /**
+     * Sets the background image for the hero section on the home page.
+     */
     const heroImageHome = document.getElementById('hero-image');
     if (heroImageHome) {
         const imageUrlHome = heroImageHome.getAttribute('data-image-url');
         heroImageHome.style.backgroundImage = `url("${imageUrlHome}")`;
     }
-
+    /**
+     * Sets the background image for the about section on the about page.
+     */
     const heroImageAbout = document.getElementById('about-image');
     if (heroImageAbout) {
         const imageUrlAbout = heroImageAbout.getAttribute('data-image-url');
         heroImageAbout.style.backgroundImage = `url("${imageUrlAbout}")`;
     }
 
-    // add `<br>` on forms before "comments" `<textarea>`
+    /**
+     * add `<br>` on forms before "comments" `<textarea>` 
+     */ 
     const commentsLabel = document.querySelector('label[for="id_comments"]');
     if (commentsLabel) {
         const lineBreak = document.createElement('br');
         commentsLabel.parentNode.insertBefore(lineBreak, commentsLabel.nextSibling);
     }
 
-    // set all `date` input fields to have a maximum date of "today", no future DOBs
+    /**
+     *  set all `date` input fields to have a maximum date of "today", no future DOBs
+     */
     document.querySelectorAll('input[type="date"]').forEach(input => {
         const today = new Date().toISOString().split("T")[0];
         input.setAttribute("max", today);
@@ -29,6 +37,9 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
+    /**
+     *  brings up a modal to for the user to click on to confirm deletions
+     */
     var deleteModal = document.getElementById('deleteConfirmModal');
     var confirmDeleteBtn = document.getElementById('confirmDelete');
     var formToSubmit;
