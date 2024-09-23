d2f0a17a3d6a5d6d16de4d124d11c2ddae8feddf
Cathal Brennan <cthlbrennan@gmail.com>
2024-09-23 18:33:55 +0000

    Refactor script.js

--- static/js/script.js
+++ static/js/script.js
@@ -61,17 +61,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const textarea = document.getElementById('id_comments');
     
     if (textarea) {
-        function adjustTextareaWidth() {
+        const adjustTextareaWidth = function() {
             if (window.innerWidth < 500) {
                 textarea.cols = 20;
             } else {
                 textarea.cols = 40;
             }
-        }
-
+        };
+      
         // Initial check and adjustment
         adjustTextareaWidth();
-
         // Add resize event listener
         window.addEventListener('resize', adjustTextareaWidth);
     }
